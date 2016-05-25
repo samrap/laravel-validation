6ca63a991e855f822ddc680c2bc67459092fe221
Sam Rapaport <sam@gigasavvy.com>
2016-05-24 17:36:09 -0700

    Add validator tests.

---
+++ tests/ValidatorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Mockery as m;
+
+use Samrap\Validation\Validator;
+
+class ValidatorTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testValidatorPasses()
+    {
+        $validator = $this->getValidator();
+
+        $rules = ['foo' => 'bar'];
+        $this->assertEmpty($validator->validate($rules)->errors());
+    }
+
+    public function testValidatorFails()
+    {
+        $errors = ['foo' => 'baz'];
+        $validator = $this->getValidator($errors);
+
+        $rules = ['foo' => 'bar'];
+        $this->assertEquals($errors, $validator->validate($rules)->errors());
+    }
+
+    protected function getValidator($errors = [])
+    {
+        $laravelValidator = m::mock('\Illuminate\Validation\Validator');
+        $laravelValidator->shouldReceive('errors')->andReturn($errors);
+
+        $factory = m::mock('Illuminate\Validation\Factory');
+        $factory->shouldReceive('make')->andReturn($laravelValidator);
+
+        $validator = new Validator($factory);
+
+        return $validator;
+    }
+}
